d90a3a24ffdaab2b7bffcc77a6bc9df5820b86a1
windyday <63159472+CoverUp137@users.noreply.github.com>
2023-11-09 02:29:58 +0800

    Update miui.sh

--- miui.sh
+++ miui.sh
@@ -6,47 +6,95 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # 恢复默认颜色
 
-
-echo -e "${GREEN}等待10秒后开始执行脚本${NC}"
-echo -e "${GREEN}接下来手机会请求获取读取权限,请点同意${NC}"
-sleep 10
-
-# 获取读写权限,给它权限
+# 提示用户必须给权限
+echo -e "${YELLOW}🚀在继续之前，请确保给予读写权限,选择Y,给给足授权🚀${NC}"
 termux-setup-storage
-echo -e "${GREEN}✅获取读写权限${NC}"
+echo -e "${GREEN}✅已获取读写权限${NC}"
 
-echo -e "${GREEN}接下来更新源${NC}"
-echo -e "${GREEN}有y的选择y然后回车,没y的直接回车${NC}"
-echo -e "${GREEN}脚本都放在github上,直链下载不了的话可能需要魔法网络${NC}"
-sleep 15
+echo  "\n"
+echo  "\n"
 
-# 更新源
-pkg update
-echo -e "${GREEN}✅已更新源${NC}"
+# 用户选择是否更新源
+echo -e "${YELLOW}😂是否要更新源？😂${NC}"
+echo -e "1. 更新源"
+echo -e "2. 跳过更新源"
+read -p "输入选项 (1 或 2)然后回车 (默认为 2): " update_option
+update_option=${update_option:-2}
 
-# 安装python和nano
-pkg install python nano wget
-echo -e "${GREEN}✅已安装python、nano和wget${NC}"
+if [ "$update_option" -eq 1 ]; then
+  # 更新源
+  echo -e "${GREEN}✅已更新源${NC}"
+  pkg update
+else
+  echo -e "${YELLOW}❗已跳过更新源❗${NC}"
+fi
 
-# 下载脚本
-wget https://raw.githubusercontent.com/CoverUp137/jiaoben/main/miui.py
-echo -e "${GREEN}✅已下载脚本${NC}"
+echo  "\n"
+echo  "\n"
+
+# 安装python
+echo -e "${YELLOW}😂是否要安装Python？😂${NC}"
+echo -e "1. 安装Python和wget"
+echo -e "2. 跳过安装"
+read -p "输入选项 (1 或 2)然后回车 (默认为 2): " install_option
+install_option=${install_option:-2}
+
+if [ "$install_option" -eq 1 ]; then
+  pkg install python wget
+  echo -e "${GREEN}✅已安装Python和wget${NC}"
+else
+  echo -e "${YELLOW}❗已跳过安装Python和wget❗${NC}"
+fi
+
+echo  "\n"
+echo  "\n"
+
+
+echo -e "${YELLOW}😂是否要下载MIUI签到脚本？😂${NC}"
+echo -e "1. 下载脚本"
+echo -e "2. 跳过下载脚本"
+read -p "输入选项 (1 或 2)然后回车 (默认为 2): " download_option
+download_option=${download_option:-2}
+
+if [ "$download_option" -eq 1 ]; then
+  # 下载脚本
+  wget https://raw.githubusercontent.com/CoverUp137/jiaoben/main/miui.py
+  echo -e "${GREEN}✅已下载脚本${NC}"
+else
+  echo -e "${YELLOW}❗已跳过下载脚本❗${NC}"
+fi
+
+echo  "\n"
+echo  "\n"
 
 # 安装python依赖
-pip install requests -i https://pypi.tuna.tsinghua.edu.cn/simple
-echo -e "${GREEN}✅已安装python依赖${NC}"
+echo -e "${YELLOW}😂是否要安装Python依赖？😂${NC}"
+echo -e "1. 安装Python依赖"
+echo -e "2. 跳过安装Python依赖"
+read -p "输入选项 (1 或 2)然后回车 (默认为 2): " install_dep_option
+install_dep_option=${install_dep_option:-2}
 
-# 用户选择单账号或多账号
+if [ "$install_dep_option" -eq 1 ]; then
+  # 安装python依赖
+  pip install requests -i https://pypi.tuna.tsinghua.edu.cn/simple
+  echo -e "${GREEN}✅已安装Python依赖${NC}"
+else
+  echo -e "${YELLOW}❗已跳过安装Python依赖❗${NC}"
+fi
+
+echo  "\n"
+echo  "\n"
+
 echo -e "${YELLOW}😂请选择账号类型😂：${NC}"
 echo -e "1. 单账号"
 echo -e "2. 多账号"
-read -p "输入选项 (1 或 2)然后回车: " account_type
+read -p "输入选项 (1 或 2)然后回车 (默认为 1): " account_type
+account_type=${account_type:-1}
 
 if [ "$account_type" -eq 1 ]; then
   read -p "📱请输入小米手机号码,输入完回车: " mi_account
   read -p "📱请输入小米密码输入,完成后回车: " mi_password
 
-  # 生成配置文件
   echo "export mi_account='$mi_account'" > config.sh
   echo "export mi_password='$mi_password'" >> config.sh
   echo "python3 miui.py" >> config.sh
@@ -56,7 +104,6 @@ elif [ "$account_type" -eq 2 ]; then
   read -p "📱请输入多个手机号码，用&分隔,完成回车: " mi_accounts
   read -p "📱请输入多个密码，用&分隔,完成回车: " mi_passwords
 
-  # 生成配置文件
   echo "export mi_account='$mi_accounts'" > config.sh
   echo "export mi_password='$mi_passwords'" >> config.sh
   echo "python3 miui.py" >> config.sh
@@ -66,8 +113,7 @@ else
   exit 1
 fi
 
-# 执行脚本
 echo -e "${YELLOW}🏃‍♀️开始执行脚本🏃‍♀️${NC}"
 sh config.sh
-# 最后添加的一句
+
 echo -e "${YELLOW}❤️以后每天只需要回到termux 并执行一次 sh config.sh即可❤️${NC}"
